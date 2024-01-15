6547706296b3769e371210c3e33574fefdc8108f
adrigm <adrigm.admin@gmail.com>
2024-01-16 00:10:29 +0100

    Draft frontend

---
+++ src/app/presentation/components/text-boxes/textMessageBox/textMessageBox.component.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-message-box',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './textMessageBox.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TextMessageBoxComponent {
+  @Input()
+  public placeholder: string = "";
+
+  @Input()
+  public disableCorrection: boolean = false;
+
+  @Output()
+  onMessageSent = new EventEmitter<string>();
+
+  public fb = inject(FormBuilder);
+  public form = this.fb.group({
+    prompt: ['', Validators.required]
+  });
+
+  handleSubmit() {
+    if (this.form.invalid) return;
+
+    const { prompt } = this.form.value;
+
+    this.onMessageSent.emit(prompt ?? "");
+    this.form.reset();
+  }
+}
---
+++ src/app/presentation/components/text-boxes/textMessageBoxFile/textMessageBoxFile.component.ts
@@ -0,0 +1,52 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+export interface TextMessageEvent {
+  prompt?: string | null;
+  file: File;
+}
+
+@Component({
+  selector: 'app-text-message-box-file',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './textMessageBoxFile.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TextMessageBoxFileComponent {
+  @Input()
+  public placeholder: string = "";
+
+  @Input()
+  public disableCorrection: boolean = false;
+
+  @Output()
+  onMessageSent = new EventEmitter<TextMessageEvent>();
+
+  public fb = inject(FormBuilder);
+  public form = this.fb.group({
+    prompt: [],
+    file: [null, Validators.required]
+  });
+
+  public file: File | undefined;
+
+  handeleSelectedFile(event: any) {
+    const file = event.target.files.item(0);
+    this.form.controls.file.setValue(file);
+    console.log(file);
+  }
+
+  handleSubmit() {
+    if (this.form.invalid) return;
+
+    const { prompt, file } = this.form.value;
+
+    this.onMessageSent.emit({ prompt, file: file! });
+    this.form.reset();
+  }
+}
